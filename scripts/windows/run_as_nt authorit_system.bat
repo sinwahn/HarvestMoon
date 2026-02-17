fd99b85a8556ab66d0d5cfd8211dbcb725875612
sinwahn <73951548+sinwahn@users.noreply.github.com>
2026-02-17 12:16:11 +0100

    Create run_as_nt authorit_system.bat
    Add a batch script to run commands as system user.

---
+++ scripts/windows/run_as_nt authorit_system.bat
@@ -0,0 +1,3 @@
+:: download and drop to Windows\System32 https://learn.microsoft.com/en-us/sysinternals/downloads/psexec -> https://download.sysinternals.com/files/PSTools.zip
+:: run with admin
+psexec -accepteula -i -s cmd.exe
