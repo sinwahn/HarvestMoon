f57f6f2568973faea8c87724c32b83e5b1620e21
sinwahn <73951548+sinwahn@users.noreply.github.com>
2026-02-18 13:11:52 +0100

    Added isinstanceof

--- js/raw/common.js
+++ js/raw/common.js
@@ -18,15 +18,14 @@ function istype(value, t0 = '', t1 = '', t2 = '', t3 = '', t4 = '', t5 = '', t6 
 }
 
 function expecttype(value, t0 = '', t1 = '', t2 = '', t3 = '', t4 = '', t5 = '', t6 = '', t7 = '') {
-	const t = typeof value
-	if (t === t0 || t === t2 || t === t3 || t === t4 || t === t5 || t === t6 || t === t7)
+	if (istype(value, t0, t1, t2, t3, t4, t5, t6, t7))
 		return value
 	// we are in the error anyway, who cares
 	const typeNames = [t0, t1, t2, t3, t4, t5, t6, t7].filter(Boolean)
-	raise_typeerror(`'${typeNames.join(" | ")}' expected, got '${t}'`)
+	raise_typeerror(`'${typeNames.join(" | ")}' expected, got '${typeof value}'`)
 }
 
-function expectinstanceof(
+function isinstanceof(
 	value,
 	c0 = undefined, c1 = undefined, c2 = undefined, c3 = undefined,
 	c4 = undefined, c5 = undefined, c6 = undefined, c7 = undefined
@@ -35,58 +34,71 @@ function expectinstanceof(
 		if (typeof c0 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c0)
-			return value
+			return true
 	}
 
 	if (c1 !== undefined) {
 		if (typeof c1 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c1)
-			return value
+			return true
 	}
 
 	if (c2 !== undefined) {
 		if (typeof c2 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c2)
-			return value
+			return true
 	}
 
 	if (c3 !== undefined) {
 		if (typeof c3 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c3)
-			return value
+			return true
 	}
 
 	if (c4 !== undefined) {
 		if (typeof c4 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c4)
-			return value
+			return true
 	}
 
 	if (c5 !== undefined) {
 		if (typeof c5 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c5)
-			return value
+			return true
 	}
 
 	if (c6 !== undefined) {
 		if (typeof c6 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c6)
-			return value
+			return true
 	}
 
 	if (c7) {
 		if (typeof c7 !== "function")
 			raise_typeerror("classes must be functions")
 		if (value instanceof c7)
-			return value
+			return true
 	}
 
+	return false
+}
+
+function expectinstanceof(
+	value,
+	c0 = undefined, c1 = undefined, c2 = undefined, c3 = undefined,
+	c4 = undefined, c5 = undefined, c6 = undefined, c7 = undefined
+) {
+	
+	if (isinstanceof(value, t0, t1, t2, t3, t4, t5, t6, t7))
+		return value
+
+	// we are in the error anyway, who cares
 	const classNames = [c0, c1, c2, c3, c4, c5, c6, c7]
 		.filter(Boolean)
 		.map(c => c.name || "<anonymous>")
